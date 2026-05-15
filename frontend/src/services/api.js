ea3dd63559cc8201a8d6a4dcf15905d590387a1c
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-05-15 13:56:43 +0530

    Added backend configuration instead of local host

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5005/api'
+  baseURL: 'https://job-portal-web-application-y35m.onrender.com/api'
 })
 
 API.interceptors.request.use((req) => {
