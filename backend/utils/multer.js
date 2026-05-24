fbb2db0588b729fc9283f1bf6d999e43ff4766a0
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-05-24 12:49:13 +0530

    Configured multer storage for proper resume file uploads

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,4 +1,15 @@
 import multer from "multer";
-const storage = multer.diskStorage({})
-const upload = multer({storage})
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'uploads/')
+    },
+
+    filename:(req,file,cb)=>{
+        cb(
+            null,
+            Date.now()+'-'+file.originalname
+        )
+    }
+})
+const upload=multer({storage})
 export default upload;
