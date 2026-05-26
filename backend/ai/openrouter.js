7ef4cf04296f37aafdfe4be60a11b131ff235c63
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-05-26 15:37:48 +0530

    Testing AI response

--- backend/ai/openrouter.js
+++ backend/ai/openrouter.js
@@ -1,37 +1,30 @@
 import axios from "axios";
 
 const generateAIResponse = async (prompt) => {
-  try {
 
-    const response = await axios.post(
-      "https://openrouter.ai/api/v1/chat/completions",
-      {
-        model: "mistralai/mistral-7b-instruct",
+  const response = await axios.post(
+    "https://openrouter.ai/api/v1/chat/completions",
+    {
+      model: "openchat/openchat-7b:free",
 
-        messages: [
-          {
-            role: "user",
-            content: prompt,
-          },
-        ],
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-          "Content-Type": "application/json",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
         },
-      }
-    );
+      ],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+        "Content-Type": "application/json",
+        "HTTP-Referer": "https://job-portal-web-application-y35m.onrender.com",
+        "X-Title": "Job Portal AI"
+      },
+    }
+  );
 
-    return response.data.choices[0].message.content;
-
-  } catch (error) {
-
-    console.log("FULL ERROR:");
-    console.log(error.response?.data);
-
-    throw error;
-}
+  return response.data.choices[0].message.content;
 };
 
 export default generateAIResponse;
