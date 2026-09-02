328ae02d1238da6f375f8f6a6b021e964fbf9673
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-02 19:54:55 +0530

    Learn google  OAuth Login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App'
 import AuthProvider from './context/AuthContext'
 import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+
       <AuthProvider >
         <App />
         <Toaster position='top-center' reverseOrder={false} />
       </AuthProvider>
+      
     </GoogleOAuthProvider>
 
   </React.StrictMode>
