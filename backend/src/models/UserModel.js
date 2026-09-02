9713497189ea1c4eabb456494aa70a5a678a64e8
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-02 19:39:41 +0530

    make password optional

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
         minlength: [6, 'password must be 6 characters']
     },
     role: {
