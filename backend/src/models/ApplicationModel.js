f5e5df7e8e8999e3aceca549d4b0e305bf56d419
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-09-05 16:49:07 +0530

    Updated Resume modal

--- backend/src/models/ApplicationModel.js
+++ backend/src/models/ApplicationModel.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
-const applicationSchema = new mongoose.Schema({
+const applicationSchema = new mongoose.Schema(
+  {
     job: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Job"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
     },
     applicant: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     resumeURL: { type: String },
     status: {
-        type: String,
-        enum:['pending','shortlisted','rejected','hired'],
-        default: "pending",
-    }
-},
-    {
-        timestamps: true
-    })
+      type: String,
+      enum: ["pending", "shortlisted", "rejected", "hired"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 // const applicationModel = mongoose.model('Application', applicationSchema);
 // export default applicationModel;
-export default mongoose.models.Application || mongoose.model("Application", applicationSchema);
+export default mongoose.models.Application ||
+  mongoose.model("Application", applicationSchema);
